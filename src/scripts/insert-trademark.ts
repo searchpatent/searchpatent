d025512a2cc08059eb817e6deb7ae5fa42630f26
devAgam <tippytactics@gmail.com>
2023-07-19 01:20:07 -0700

    simplified insert trade mark function

--- src/scripts/insert-trademark.ts
+++ src/scripts/insert-trademark.ts
@@ -2,28 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function addTradeMark(
-  name: string,
-  description: string,
-  stateRegisterationNumber: number,
-  applicationNumber: number,
-  applicationDate: string,
-  registrationDate: string,
-  registrationExpiryDate: string,
-  imageUrlOrigin: string,
-  copyRightOwner: string
-) {
+export async function addTradeMark(data: any) {
   return await prisma.intellectualProperty.create({
     data: {
-      name,
-      description,
-      stateRegisterationNumber,
-      applicationNumber,
-      applicationDate,
-      registrationDate,
-      registrationExpiryDate,
-      imageUrlOrigin,
-      copyRightOwner,
+      ...data,
     },
   });
 }
