0e732c83690ece5aa65b33c969c78201f3ebedcd
devAgam <tippytactics@gmail.com>
2023-07-22 16:50:09 -0700

    boilerplate for a proper rest API

--- app.ts
+++ app.ts
@@ -1,53 +1,99 @@
-import puppeteer from "puppeteer";
-import { extractTrademarkInformation } from "./src/scrappers/fips.ru";
-import { addTradeMark } from "./src/scripts/insert-trademark";
-import { parseCsv } from "./src/scripts/parse-csv";
-import * as fs from "fs";
-import { downloadLatestDatasetOrigin } from "./src/scripts/get-latest-excel";
-import { downloadFileAndStoreByUrl } from "./src/scripts/download-and-store-file";
-import { v4 as uuid } from "uuid";
-async function test(from: number, to: number) {
-  // read the sample dataset and get the docuemntId from index 0 and then log the documentId
-  const sampleDatasetPath = "./files/latest-dataset-origin.csv";
-  // check if the file exists
-  if (!fs.existsSync(sampleDatasetPath)) {
-    await downloadLatestDatasetOrigin();
+import express, { Request, Response, Application } from "express";
+const app: Application = express();
+const PORT = process.env.PORT || 8000;
+
+import expressSession from "express-session";
+import connectRedis from "connect-redis";
+import { createClient } from "redis";
+import passport from "passport";
+import cors from "cors";
+import { routes } from "./src/routes";
+require("./src/api/components/auth/passport");
+
+// create a var called domain which takes value from ORIGIN env variable and remove the protocol and the port
+const domain = process.env.ORIGIN
+  ? process.env.ORIGIN.replace(/(^\w+:|^)\/\//, "").split(":")[0]
+  : "";
+var whitelist = [
+  process.env.FRONTEND_URL,
+  process.env.CUSTOMER_URL,
+  process.env.ORIGIN,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin as string) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin as string) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use((req: any, res: any, next: any) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+
+  // to be allowed origin is the origin of the request if it is in the whitelist
+
+  const toBeAllowedOrigin = req.headers.origin;
+
+  if (!toBeAllowedOrigin) return next();
+
+  if (whitelist.indexOf(toBeAllowedOrigin) !== -1) {
+    res.header("Access-Control-Allow-Origin", toBeAllowedOrigin);
+    next();
+    return;
   }
 
-  // launch the browser
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
+  // terminatre the request if the origin is not in the whitelist
+  res.sendStatus(401);
+});
+const redisClient = createClient({ legacyMode: true });
+redisClient.connect().catch((err) => {
+  throw err;
+});
 
-  // parse the csv file and get the documentIds
-  const docIds: number[] = await parseCsv(sampleDatasetPath, from, to);
-  console.info("🚀 Starting scraping");
-  for (let i = 0; i < docIds.length; i++) {
-    // wait 3 seconds to avoid getting blocked by the website, rate limit is 3 second between each request from same host
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+const RedisStore = connectRedis(expressSession);
 
-    const scrapedData = await extractTrademarkInformation(
-      browser,
-      "https://new.fips.ru/registers-doc-view/fips_servlet?DB=RUTM&DocNumber=" +
-        docIds[i]
-    );
-    if (!scrapedData) {
-      return;
-    }
-    const ext = scrapedData.imageOrigin.split(".").pop();
-    const fileId = uuid() + "." + ext;
-    await downloadFileAndStoreByUrl(fileId, scrapedData.imageOrigin);
-    await addTradeMark({
-      ...scrapedData,
-      imageIdLocal: fileId,
-    });
-    // if i === length - 1, close the browser
-    if (i === docIds.length - 1) {
-      console.info("✅ Scraping completed");
-      await browser.close();
-    }
-  }
-}
+app.use(
+  expressSession({
+    store: new RedisStore({ client: redisClient }),
 
-// call the function
-test(2000, 5);
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      domain: domain,
+      // domain: "." + process.env.DOMAIN,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+routes(app);
+
+app.listen(PORT, (): void => {
+  console.log(`Server Running here 👉 https://localhost:${PORT}`);
+});
